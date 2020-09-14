abc4129031c883f0a05039b715de39ff8c2b8c60
mnegura <msnegura@gmail.com>
2020-09-14 21:08:31 +0300

    project init + hompage init

---
+++ src/homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./homepage.scss";
+
+ const HomePage = () => (
+   <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+      <div class="content">
+      <span className="title">hats</span>
+      <span className="subtitle">shop now</span>
+      </div>
+      </div>
+    </div>
+   </div>
+ );
+
+ export default HomePage;
